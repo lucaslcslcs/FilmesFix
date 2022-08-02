707fc2d7bd4387f834fba777481eff6c60b442d6
lucas <lucaslcslcs1998@gmail.com>
2022-08-02 10:55:56 -0300

    lista de dados fake de filmes

--- app/src/main/java/com/br/natanfc/filmesflix/MainActivity.kt
+++ app/src/main/java/com/br/natanfc/filmesflix/MainActivity.kt
@@ -2,10 +2,42 @@ package com.br.natanfc.filmesflix
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.br.natanfc.filmesflix.model.Movie
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private val listOfMovies = arrayListOf(
+        Movie(
+            id = 0,
+            titulo = "Titanic",
+            descricao = null,
+            imagem = null.toString(),
+            dataLancamento = null
+        ),
+        Movie(
+            id = 1,
+            titulo = "Central do Brasil",
+            descricao = null,
+            imagem = null.toString(),
+            dataLancamento = null
+        ),
+
+    )
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        populateList()
     }
+
+    private fun populateList() {
+        moviesList.apply {
+            hasFixedSize()
+            adapter = MoviesAdapter(listOfMovies)
+        }
+    }
+
 }
